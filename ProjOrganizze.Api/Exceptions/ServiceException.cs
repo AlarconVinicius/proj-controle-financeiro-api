69afb5b56af1ac2ec9a7d9524df2452180ae079d
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-06-03 11:12:59 -0300

    Adicionando ServiceException

---
+++ ProjOrganizze.Api/Exceptions/ServiceException.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjOrganizze.Api.Exceptions
+{
+    public class ServiceException : Exception
+    {
+        public ServiceException(string message) : base(message)
+        {
+        }
+    }
+}
