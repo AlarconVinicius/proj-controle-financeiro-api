82dff988b9657d931e2ed96179a6fc3143182edf
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-06-23 12:21:24 -0300

    Adicionando método PagarFatura e refatorando os Gets

--- ProjControleFinanceiro.Api/Controllers/FaturaController.cs
+++ ProjControleFinanceiro.Api/Controllers/FaturaController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjControleFinanceiro.Data.Repositorios;
 using ProjControleFinanceiro.Domain.DTOs.Fatura;
 using ProjControleFinanceiro.Domain.Extensions;
 using ProjControleFinanceiro.Domain.Interfaces.Repositorios;
+using ProjControleFinanceiro.Domain.Interfaces.Services;
+using ProjControleFinanceiro.Entities.Entidades;
+using ProjControleFinanceiro.Entities.Entidades.Enums;
 
 namespace ProjControleFinanceiro.Api.Controllers
 {
@@ -10,21 +14,61 @@ namespace ProjControleFinanceiro.Api.Controllers
     public class FaturaController : MainController
     {
         private readonly IFaturaRepository _faturaRepository;
-        public FaturaController(IFaturaRepository faturaRepository)
+        private readonly ICartaoService _cartaoRepository;
+        public FaturaController(IFaturaRepository faturaRepository, ICartaoService cartaoRepository)
         {
             _faturaRepository = faturaRepository;
+            _cartaoRepository = cartaoRepository;
         }
-        [HttpGet]
-        public async Task<IActionResult> ListarFaturas()
+        [HttpGet("cartao/{cartaoId}")]
+        public async Task<IActionResult> ObterFaturas(int cartaoId)
         {
-            var objetosDb = await _faturaRepository.ObterFaturas();
+            var objetosDb = await _faturaRepository.ObterFaturas(cartaoId);
             List<FaturaViewDTO> objetosMapeados = new List<FaturaViewDTO>();
-            foreach (var objetoDb in objetosDb)
+            //foreach (var objetoDb in objetosDb)
+            //{
+            //    objetosMapeados.Add(objetoDb.ToGetDTO());
+            //}
+            return CustomResponse(objetosDb.Select(x => x.ToGetDTO()));
+        }
+        [HttpGet("{faturaId}")]
+        public async Task<IActionResult> ObterFaturaPorId(int faturaId)
+        {
+            var faturaDb = await _faturaRepository.ObterFaturaPorId(faturaId);
+            if (faturaDb == null)
             {
-                objetosMapeados.Add(objetoDb.ToGetDTO());
-                //objetosMapeados.Add(_faturaMapping.MapToGetDTO(objetoDb));
+                AdicionarErroProcessamento("Fatura não encontrada.");
+                return CustomResponse();
             }
-            return Ok(objetosMapeados);
+            return CustomResponse(faturaDb.ToGetDTO());
+        }
+        [HttpPost]
+        public async Task<IActionResult> PagarFatura(int cartaoId, int faturaId)
+        {
+            var cartaoDb = await _cartaoRepository.ObterCartaoPorId(cartaoId);
+            if (cartaoDb == null)
+            {
+                AdicionarErroProcessamento("Cartão não encontrado.");
+                return CustomResponse();
+            }
+            var faturaDb = await _faturaRepository.ObterFaturaPorId(faturaId);
+            if (faturaDb == null)
+            {
+                AdicionarErroProcessamento("Fatura não encontrada.");
+                return CustomResponse();
+            }
+            if(faturaDb.StatusPagamento == StatusPagamento.Pago)
+            {
+                AdicionarErroProcessamento("Não é possível realizar o pagamento, pois a fatura já está paga.");
+                return CustomResponse();
+            }
+            bool result = await _faturaRepository.PagarFatura(cartaoId, faturaId);
+            if (!result)
+            {
+                AdicionarErroProcessamento("Falha ao tentar pagar fatura.");
+                return CustomResponse();
+            }
+            return CustomResponse();
         }
     }
 }
--- ProjControleFinanceiro.Domain/Interfaces/Repositorios/IFaturaRepository.cs
+++ ProjControleFinanceiro.Domain/Interfaces/Repositorios/IFaturaRepository.cs
@@ -4,9 +4,11 @@ namespace ProjControleFinanceiro.Domain.Interfaces.Repositorios
 {
     public interface IFaturaRepository : IBaseRepository<Fatura>
     {
-        Task<List<Fatura>> ObterFaturas();
-        Task<Fatura> ObterFaturaPorCartaoMesAno(int cartaoId, int mes, int ano);
         Task AdicionarFaturas(Cartao objeto);
+        Task<List<Fatura>> ObterFaturas(int cartaoId); // Ajustar para, ObterFaturasPorCartaoId
+        Task<Fatura> ObterFaturaPorId(int faturaId, int cartaoId = 0);
+        Task<Fatura> ObterFaturaPorCartaoMesAno(int cartaoId, int mes, int ano);
+        Task<bool> PagarFatura(int cartaoId, int faturaId);
         Task DeletarFatura(int id);
 
     }
