812f66bd87335755552d988fe04e28bfa36d34e6
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-06-29 13:27:18 -0300

    Refatorando DTOs

--- ProjControleFinanceiro.Domain/DTOs/Transacao/TransacaoUpdDTO.cs
+++ ProjControleFinanceiro.Domain/DTOs/Transacao/TransacaoUpdDTO.cs
@@ -3,19 +3,14 @@ using System.ComponentModel;
 
 namespace ProjControleFinanceiro.Domain.DTOs.Transacao
 {
-    public class TransacaoAddDTO
+    public class TransacaoUpdDTO
     {
+        public int Id { get; set; }
         public string Descricao { get; set; }
         public double Valor { get; set; }
         [DefaultValue("dd/MM/yyyy")]
         public string Data { get; set; }
         public TipoTransacao TipoTransacao { get; set; }
         public Categoria Categoria { get; set; }
-        [DefaultValue(false)]
-        public bool Pago { get; set; }
-        [DefaultValue(false)]
-        public bool Repete { get; set; }
-        public int QtdRepeticao { get; set; }
-
     }
 }
--- ProjControleFinanceiro.Domain/DTOs/Transacao/TransacaoViewDTO.cs
+++ ProjControleFinanceiro.Domain/DTOs/Transacao/TransacaoViewDTO.cs
@@ -5,11 +5,11 @@ namespace ProjControleFinanceiro.Domain.DTOs.Transacao
     public class TransacaoViewDTO
     {
         public int Id { get; set; }
-        public MetodoPagamentoDTO MetodoPagamento { get; set; }
         public string Descricao { get; set; }
         public double Valor { get; set; }
         public string Data { get; set; }
         public TipoTransacaoDTO TipoTransacao { get; set; }
         public CategoriaDTO Categoria { get; set; }
+        public bool Pago { get; set; }
     }
 }
