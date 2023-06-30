f2f5f28f777051b96d376ea0fbe5e51adcac8f47
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-06-29 23:45:06 -0300

    Configurar Respostas padroes

---
+++ ProjControleFinanceiro.Api/Controllers/Configuracao/ApiErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace ProjControleFinanceiro.Api.Controllers.Configuracao
+{
+    public class ApiErrorResponse
+    {
+        [DefaultValue(false)]
+        public bool Success { get; set; }
+        public IDictionary<string, string[]> Errors { get; set; }
+        public ApiErrorResponse()
+        {
+            Errors = new Dictionary<string, string[]>
+            {
+                { "Mensagens", new string[0] }
+            };
+        }
+    }
+}
---
+++ ProjControleFinanceiro.Api/Controllers/Configuracao/ApiSuccessResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace ProjControleFinanceiro.Api.Controllers.Configuracao
+{
+    public class ApiSuccessResponse<T>
+    {
+        public bool Success { get; set; }
+        [DefaultValue(null)]
+        public T Data { get; set; }
+    }
+}
