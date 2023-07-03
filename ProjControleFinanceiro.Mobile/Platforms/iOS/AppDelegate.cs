36b96e70e32e29e1f8eab399de669cce588650e4
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-07-03 18:03:26 -0300

    Adicionando ProjMobile

---
+++ ProjControleFinanceiro.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProjControleFinanceiro.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
