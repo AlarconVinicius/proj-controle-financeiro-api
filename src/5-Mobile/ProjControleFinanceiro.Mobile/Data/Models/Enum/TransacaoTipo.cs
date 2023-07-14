582ff36a37aef173a0a796340a833d8922d70645
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-07-14 10:58:17 -0300

    Adicionar Models

---
+++ src/5-Mobile/ProjControleFinanceiro.Mobile/Data/Models/Enum/TransacaoTipo.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjControleFinanceiro.Mobile.Data.Models.Enum
+{
+    public enum TransacaoTipo
+    {
+        Income,
+        Expense
+    }
+}
---
+++ src/5-Mobile/ProjControleFinanceiro.Mobile/Data/Models/TransacaoModel.cs
@@ -0,0 +1,21 @@
+﻿using LiteDB;
+using ProjControleFinanceiro.Mobile.Data.Models.Enum;
+
+namespace ProjControleFinanceiro.Mobile.Data.Models
+{
+    public class TransacaoModel
+    {
+        [BsonId]
+        public int Id { get; set; }
+        public TransacaoTipo Type { get; set; }
+        public String Name { get; set; }
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+        public bool Paid { get; set; }
+        public bool Repete { get; set; }
+        public int QtdRepete { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
