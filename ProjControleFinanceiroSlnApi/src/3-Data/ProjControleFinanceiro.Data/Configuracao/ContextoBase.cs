67dff73c90c00bd83068d64bf7dfc369c7142be7
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2023-08-19 15:32:29 -0300

    Padronizar Namespaces e Classes - Data

--- ProjControleFinanceiroSlnApi/src/3-Data/ProjControleFinanceiro.Data/Configuracao/ContextoBase.cs
+++ ProjControleFinanceiroSlnApi/src/3-Data/ProjControleFinanceiro.Data/Configuracao/ContextoBase.cs
@@ -2,30 +2,29 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ProjControleFinanceiro.Entities.Entidades;
 
-namespace ProjControleFinanceiro.Data.Configuracao
+namespace ProjControleFinanceiro.Data.Configuracao;
+
+public class ContextoBase : DbContext
 {
-    public class ContextoBase : DbContext
+    public ContextoBase(DbContextOptions<ContextoBase> options) : base(options)
+    { }
+
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Transacao> Transacoes { get; set; }
+
+}
+public class FornecedorMapping : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> builder)
     {
-        public ContextoBase(DbContextOptions<ContextoBase> options) : base(options)
-        { }
+        builder.HasKey(c => c.Id);
 
-        public DbSet<Cliente> Clientes { get; set; }
-        public DbSet<Transacao> Transacoes { get; set; }
-  
-    }
-    public class FornecedorMapping : IEntityTypeConfiguration<Cliente>
-    {
-        public void Configure(EntityTypeBuilder<Cliente> builder)
-        {
-            builder.HasKey(c => c.Id);
+        //1 : N
+        builder.HasMany(c => c.transacoes)
+            .WithOne(t => t.cliente)
+            .HasForeignKey(t => t.ClienteId);
 
-            //1 : N
-            builder.HasMany(c => c.transacoes)
-                .WithOne(t => t.cliente)
-                .HasForeignKey(t => t.ClienteId);
+       builder.ToTable("clientes");
 
-           builder.ToTable("clientes");
-
-        }
     }
 }
