ade6fea7a8145e208db9d1e8ac3157dfe71657ec
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2023-08-20 07:37:29 -0300

    Configurar o Identity

---
+++ ProjControleFinanceiroSlnApi/src/5-Identity/ProjControleFinanceiro.Identity/Auth/CustomAuthorization.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ProjControleFinanceiro.Identity.Auth;
+
+public static class CustomAuthorization
+{
+    public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+    {
+        return context.User.Identity!.IsAuthenticated && context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+    }
+}
---
+++ ProjControleFinanceiroSlnApi/src/5-Identity/ProjControleFinanceiro.Identity/Auth/RequisitoClaimFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ProjControleFinanceiro.Identity.Auth;
+
+public class RequisitoClaimFilter : IAuthorizationFilter
+{
+    private readonly Claim _claim;
+
+    public RequisitoClaimFilter(Claim claim)
+    {
+        _claim = claim;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!context.HttpContext.User.Identity!.IsAuthenticated)
+        {
+            context.Result = new StatusCodeResult(401);
+            return;
+        }
+
+        if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+        {
+            context.Result = new StatusCodeResult(403);
+        }
+
+    }
+}
