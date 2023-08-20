4d5d7f94f1a4d3b33dedde4e5ace83484c335fdd
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2023-08-20 07:33:19 -0300

    Remover configurações do Identity do Projeto Api

--- ProjControleFinanceiroSlnApi/src/1-Api/ProjControleFinanceiro.Api/Configuration/Auth/CustomAuthorization.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjControleFinanceiro.Api.Configuration.Auth;
-
-public static class CustomAuthorization
-{
-    public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
-    {
-        return context.User.Identity!.IsAuthenticated && context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
-    }
-}
