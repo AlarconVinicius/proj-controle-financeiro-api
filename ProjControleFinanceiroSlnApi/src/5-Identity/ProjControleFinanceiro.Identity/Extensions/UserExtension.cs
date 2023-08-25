8c353432b4c65b37b09491c69559ee936bb38ca3
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-08-25 17:57:19 -0300

    Adicionar UserExtension no Proj. Identity

---
+++ ProjControleFinanceiroSlnApi/src/5-Identity/ProjControleFinanceiro.Identity/Extensions/UserExtension.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using ProjControleFinanceiro.Domain.DTOs.Usuario;
+using ProjControleFinanceiro.Entities.Entidades;
+
+namespace ProjControleFinanceiro.Identity.Extensions;
+public static class UserExtension
+{
+    public static IdentityUser ToAddClienteIdentity(this AddUserRequest value)
+    {
+        return new IdentityUser
+        {
+            UserName = value.Email,
+            Email = value.Email,
+            EmailConfirmed = true,
+            PhoneNumber = value.PhoneNumber,
+            PhoneNumberConfirmed = true,
+            NormalizedEmail = value.Email.ToUpper(),
+            NormalizedUserName = value.Email.ToUpper()
+        };
+    }
+    public static Cliente ToAddClienteDto(this AddUserRequest value, Guid id)
+    {
+        return new Cliente()
+        {
+            Id = id,
+            Name = value.Name,
+            LastName = value.LastName
+        };
+    }
+    public static UserResponse ToViewUsuarioDto(this Cliente value, IdentityUser valueIdentity, List<string> roles)
+    {
+        return new UserResponse(value.Id, value.Name, value.LastName, valueIdentity.Email!, valueIdentity.PhoneNumber!, roles);
+    }
+}
