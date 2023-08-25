8fa64a8399e44f0cc702d11807bdb0d18fb8080a
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-08-25 18:19:15 -0300

    Adicionar UsuarioController

---
+++ ProjControleFinanceiroSlnApi/src/1-Api/ProjControleFinanceiro.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ProjControleFinanceiro.Api.Controllers.Configuracao;
+using ProjControleFinanceiro.Domain.DTOs.Usuario;
+using ProjControleFinanceiro.Identity.Domain.Interfaces;
+
+namespace ProjControleFinanceiro.Api.Controllers;
+
+[Route("api/usuarios")]
+public class UsuarioController : MainController
+{
+    private readonly IUsuarioService _usuarioService;
+    public UsuarioController(IUsuarioService usuarioService)
+    {
+        _usuarioService = usuarioService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<UserResponse> ObterUsuarioPorId(Guid id)
+    {
+        return await _usuarioService.ObterUsuarioPorId(id);
+    }
+    [HttpGet]
+    public async Task<IEnumerable<UserResponse>> ObterUsuarios()
+    {
+        return await _usuarioService.ObterUsuarios();
+    }
+}
