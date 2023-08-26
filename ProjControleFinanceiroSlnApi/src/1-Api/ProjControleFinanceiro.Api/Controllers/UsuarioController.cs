30582861bc4827132aef7705d019b257d2183a9f
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-08-26 20:12:54 -0300

    Adicionar endpoint AtualizarUsuario

--- ProjControleFinanceiroSlnApi/src/1-Api/ProjControleFinanceiro.Api/Controllers/UsuarioController.cs
+++ ProjControleFinanceiroSlnApi/src/1-Api/ProjControleFinanceiro.Api/Controllers/UsuarioController.cs
@@ -1,17 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 using ProjControleFinanceiro.Api.Controllers.Configuracao;
 using ProjControleFinanceiro.Domain.DTOs.Usuario;
+using ProjControleFinanceiro.Domain.Helpers;
 using ProjControleFinanceiro.Identity.Domain.Interfaces;
 
 namespace ProjControleFinanceiro.Api.Controllers;
 
 [Route("api/usuarios")]
+[Authorize]
 public class UsuarioController : MainController
 {
+    private readonly IHttpContextAccessor _accessor;
     private readonly IUsuarioService _usuarioService;
-    public UsuarioController(IUsuarioService usuarioService)
+    public UsuarioController(IHttpContextAccessor accessor, IUsuarioService usuarioService)
     {
+        _accessor = accessor;
         _usuarioService = usuarioService;
     }
 
@@ -24,5 +29,15 @@ public class UsuarioController : MainController
     public async Task<IEnumerable<UserResponse>> ObterUsuarios()
     {
         return await _usuarioService.ObterUsuarios();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> AtualizarUsuario(UpdUserRequest objeto)
+    {
+
+        Guid idUsuarioLogado = UsuarioHelper.GetUserId(_accessor);
+        await _usuarioService.AtualizarUsuario(idUsuarioLogado, objeto);
+        if (!_usuarioService.OperacaoValida()) return CustomResponse(_usuarioService.GetErrors());
+        return CustomResponse();
     }
 }
