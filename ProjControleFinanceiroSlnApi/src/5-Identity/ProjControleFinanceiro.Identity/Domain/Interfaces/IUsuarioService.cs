be4bb4a4e285bf3d657f152176d3e5c5dd787f65
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-08-26 20:11:01 -0300

    Adicionar método AtualizarUsuario

--- ProjControleFinanceiroSlnApi/src/5-Identity/ProjControleFinanceiro.Identity/Domain/Interfaces/IUsuarioService.cs
+++ ProjControleFinanceiroSlnApi/src/5-Identity/ProjControleFinanceiro.Identity/Domain/Interfaces/IUsuarioService.cs
@@ -1,8 +1,10 @@
 ﻿using ProjControleFinanceiro.Domain.DTOs.Usuario;
+using ProjControleFinanceiro.Domain.Interfaces.Services;
 
 namespace ProjControleFinanceiro.Identity.Domain.Interfaces;
-public interface IUsuarioService
+public interface IUsuarioService : IMainService
 {
     Task<UserResponse> ObterUsuarioPorId(Guid id);
     Task<IEnumerable<UserResponse>> ObterUsuarios();
+    Task AtualizarUsuario(Guid idUsuarioLogado, UpdUserRequest objeto);
 }
--- ProjControleFinanceiroSlnApi/src/5-Identity/ProjControleFinanceiro.Identity/Domain/Services/UsuarioService.cs
+++ ProjControleFinanceiroSlnApi/src/5-Identity/ProjControleFinanceiro.Identity/Domain/Services/UsuarioService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using FluentValidation;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 using ProjControleFinanceiro.Domain.DTOs.Usuario;
@@ -11,18 +13,66 @@ using ProjControleFinanceiro.Identity.Extensions;
 namespace ProjControleFinanceiro.Identity.Domain.Services;
 public class UsuarioService : MainService, IUsuarioService
 {
-    private readonly IUsuarioRepository _clienteRepository;
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly IValidator<UpdUserRequest> _updValidator;
+    private readonly IUsuarioRepository _usuarioRepository;
 
-    public UsuarioService(IUsuarioRepository clienteRepository, UserManager<IdentityUser> userManager)
+    public UsuarioService(IUsuarioRepository usuarioRepository, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IValidator<UpdUserRequest> updValidator)
     {
-        _clienteRepository = clienteRepository;
         _userManager = userManager;
+        _usuarioRepository = usuarioRepository;
+        _updValidator = updValidator;
+    }
+
+    public async Task AtualizarUsuario(Guid idUsuarioLogado, UpdUserRequest objeto)
+    {
+        try
+        {
+            var validationResult = await _updValidator.ValidateAsync(objeto);
+            if (!validationResult.IsValid)
+            {
+                AdicionarErroProcessamento(validationResult);
+                return;
+            }
+            Cliente usuarioDb = await _usuarioRepository.GetEntityByIdAsync(objeto.Id);
+            var usuarioIdentityDb = await _userManager.FindByIdAsync(objeto.Id.ToString());
+            if (usuarioIdentityDb is null && usuarioDb is null)
+            {
+                AdicionarErroProcessamento("Usuário não encontrado.");
+                return;
+            }
+            var usuarioLogado = await _userManager.FindByIdAsync(idUsuarioLogado.ToString());
+            if (usuarioLogado is null)
+            {
+                AdicionarErroProcessamento("Usuário logado não encontrado.");
+                return;
+            }
+            var userRoles = (await _userManager.GetRolesAsync(usuarioLogado)).ToList();
+            if (!userRoles.Contains(Roles.Admin) && objeto.Id != idUsuarioLogado)
+            {
+                AdicionarErroProcessamento("Operação não permitida.");
+                return;
+            }
+            usuarioDb.Name = objeto.Name;
+            usuarioDb.LastName = objeto.LastName;
+            usuarioIdentityDb!.UserName = objeto.Email;
+            usuarioIdentityDb.Email = objeto.Email;
+            usuarioIdentityDb.PhoneNumber = objeto.PhoneNumber;
+            usuarioIdentityDb.NormalizedEmail = objeto.Email.ToUpper();
+            usuarioIdentityDb.NormalizedUserName = objeto.Email.ToUpper();
+
+            await _usuarioRepository.UpdateAsync(usuarioDb);
+            await _userManager.UpdateAsync(usuarioIdentityDb);
+        }
+        catch (Exception ex)
+        {
+            AdicionarErroProcessamento($"Erro ao registrar usuário: {ex.Message}");
+        }
     }
 
     public async Task<UserResponse> ObterUsuarioPorId(Guid id)
     {
-        Cliente usuarioDb = await _clienteRepository.GetEntityByIdAsync(id);
+        Cliente usuarioDb = await _usuarioRepository.GetEntityByIdAsync(id);
         var usuarioIdentityDb = await _userManager.FindByIdAsync(id.ToString());
 
         if (usuarioIdentityDb is null && usuarioDb is null) 
@@ -36,7 +86,7 @@ public class UsuarioService : MainService, IUsuarioService
 
     public async Task<IEnumerable<UserResponse>> ObterUsuarios()
     {
-        List<Cliente> usuariosDb = await _clienteRepository.ListAsync();
+        List<Cliente> usuariosDb = await _usuarioRepository.ListAsync();
         List<IdentityUser> usuariosIdentityDb = await _userManager.Users.ToListAsync();
 
         var usuariosResponses = new List<UserResponse>();
